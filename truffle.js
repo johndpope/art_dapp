7767b9b855a4f9dc1c9a192bf0d4950fde8d4090
Duncan Cock Foster <dcarlcf@gmail.com>
2018-04-21 09:52:59 -0700

    Edited iPad image filed

--- truffle.js
+++ truffle.js
@@ -1,11 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var infura_apikey = "LaHsmpJShSJIEAQiI0bg";
+var mnemonic = "stock improve rough range dutch syrup trust horse drastic involve moral film";
+
+// TODO: Create a configuration file that doesn't get pushed to cache this
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+  },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 4612388
     }
   }
 };
