3c4b75c841b5e586265b6689e4e8739cdaa30dae
Saahith Pochiraju <saahith116@gmail.com>
2018-04-21 13:30:05 -0400

    fixed scrolling, created pages for layout

--- server/main/views.py
+++ server/main/views.py
@@ -11,3 +11,19 @@ main_blueprint = Blueprint('main', __name__,)
 @main_blueprint.route("/", methods=['GET', 'POST'])
 def landing():
     return render_template('main/landing.html')
+
+@main_blueprint.route("/marketplace", methods=['GET', 'POST'])
+def marketplace():
+    return render_template('main/marketplace.html')
+
+@main_blueprint.route("/faq", methods=['GET', 'POST'])
+def faq():
+    return render_template('main/faq.html')
+
+@main_blueprint.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template('main/about.html')
+
+@main_blueprint.route("/join", methods=['GET', 'POST'])
+def join():
+    return render_template('main/join.html')
